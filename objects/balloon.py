3c711849f4e79ce211eae82be7b4af66946bd3ee
Gabriel <teigne.gabriel@gmail.com>
2024-11-22 15:35:20 +0100

    Add balloon class

--- objects/balloon.py
+++ objects/balloon.py
@@ -4,9 +4,12 @@
 from objects import Cell
 
 class Balloon:
-    def __init__(self) -> None:
-        self.cell: Cell
-        self.alt: int
+    def __init__(self, startCell: Cell) -> None:
+        self.cell: Cell = startCell
+        self.alt: int = 0
+        self.altMax = len(self.cell._winds)
 
-    def moveAlt(self) -> None:
-        raise NotImplementedError
+    def moveAlt(self, value: int) -> None:
+        assert value==1 or value==0 or value==-1
+        assert self.altMax > self.alt+value > 0
+        self.alt += value
