28f7d3aa0a82763f8f50954ef003a185136773c2
Tom Daniaud <tom.daniaud@etu.univ-nantes.fr>
2024-11-22 15:25:28 +0100

    ajout de la methode <print> de CellMap

--- cellMap.py
+++ cellMap.py
@@ -4,9 +4,10 @@
 """Module.
 """
 
+from __future__ import annotations
 from cells import Cell, TargetCell
 from wind import Wind
-from __future__ import annotations
+from polyparser import ParserData, parseChallenge
 
 class CellMap:
     """ Cette class représente la carte des vents sous forme d'un graph de Cells.
@@ -32,40 +33,59 @@ class CellMap:
         """Initialisation de la matrice de cellule"""
 
         for row in range(self.rows):
-            for col in range(self.cols):
+            for col in range(self.columns):
 
                 winds = []
                 for vec in parserData.winds:
                     winds.append(Wind(vec[0], vec[1]))
 
                 if (row, col) in parserData.targets_pos:
-                    curCell = TargetCell(row, col, tuple(winds))
+                    cell = TargetCell(row, col, tuple(winds))
                 else:
-                    curCell = Cell(row, col, tuple(winds))
+                    cell = Cell(row, col, tuple(winds))
 
-                if (curCell.x, curCell.y) == parserData.starting_cell:
-                    self.startingCell = curCell
+                if (cell.x, cell.y) == parserData.starting_cell:
+                    self.startingCell = cell
 
-                self.map.append(curCell)
+                self.map.append(cell)
 
     
-    def defineTargetRange(self, parserData: ParserData) -> None:
+    def defineTargetRange(self, parserData: ParserData) -> None: #test pas fait !!
         """Defini les cellules en périhérie d'une target"""
         for targetPos in parserData.targets_pos:
             target = self.map[targetPos.y][targetPos.x]
-            for row in range(target.y-self.radius, target.y+self.radius+1):
+            for row in range(target.x-self.radius, target.x+self.radius+1):
                 for col in range(target.y-self.radius, target.y+self.radius+1):
-                    curCell:Cell = self.map[row][col]
-                    if self.inRange(target, curCell):
-                        curCell.addTarget(target)
+                    cell:Cell = self.map[row][col]
+                    if self.inRange(target, cell):
+                        cell.addTarget(target)
                 
             
             
 
     def inRange(self, target:TargetCell, cell:Cell) -> bool:
+        """Retourne si la cellule est dans le rayon <self.radius> de target"""
         columndist = lambda c1, c2: min(abs(c1-c2), self.columns - abs(c1-c2))
         return (cell.x - target.x)**2 + columndist(cell.y, target.y)**2 <= self.radius
+    
+    def print(self, altitude:int):  #pour l'instant affiche les celulles et les targets
+        """Affiche la carte des vents à l'altitude donnée"""
 
+        assert 0 < altitude < self.altitudes, f"L'altitude {altitude} n'existe pas"
+
+        print(f"Affichage de la carte de taille {self.rows}x{self.columns}\n\n")
+        for row in range(self.rows):
+            for col in range(self.columns):
+                cell:Cell = self.map[row][col]
+                if cell.pos == self.startingCell:
+                    print("s", end="")
+                elif type(cell) == TargetCell:
+                    print("T", end="")
+                elif len(cell.targets) > 0:
+                    print("P", end="")
+                else:
+                    print("_", end="")
+            print("\n")
 
     
     def save(fileName:str):
@@ -73,3 +93,10 @@ class CellMap:
     
     def load(fileName:str) -> CellMap:
         ...
+
+
+if __name__ == "__main__":
+    cellMap = CellMap(parseChallenge("/challenges/a_example.in"))
+    cellMap.print()
+
+
