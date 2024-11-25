cb42ccee2eed226249e7ba2b858eefea00c51b84
Gabriel <teigne.gabriel@gmail.com>
2024-11-25 16:32:34 +0100

    Add wind class doc

--- objects/wind.py
+++ objects/wind.py
@@ -4,6 +4,7 @@
 
 class Wind:
     def __init__(self, dx: int, dy: int) -> None:
+        """Entity that describe the wind with a vec"""
         self.vec: tuple[int,int] = (dx,dy)
         self.dx: int = dx
         self.dy: int = dy
