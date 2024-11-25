13977d65f3132d3fc46078e23928550f204e9fc8
Gabriel <teigne.gabriel@gmail.com>
2024-11-25 15:04:54 +0100

    Add targetCell class test

--- objects/targetCell.py
+++ objects/targetCell.py
@@ -8,13 +8,14 @@ from .wind import Wind
 class TargetCell(Cell):
     def __init__(self, x: int, y: int, winds: list[Wind]) -> None:
         super().__init__(x, y, winds)
-        self.coverBy: set[Balloon]
-        self._points: int
+        self.coverBy: set[Balloon] = set()
+        self._points: int = 0
 
     def addBalloon(self, balloon: Balloon) -> None:
         self.coverBy.add(balloon)
 
     def removeBalloon(self, balloon: Balloon) -> None:
+        assert balloon in self.coverBy
         self.coverBy.remove(balloon)
 
     def isCovered(self) -> bool:
---
+++ tests/test_targetCell.py
@@ -0,0 +1,58 @@
+import pytest
+from objects import TargetCell, Wind, Balloon
+
+class TestTargetCell:
+
+    def test_targetCell_init(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        assert type(cell) == TargetCell
+        assert len(cell.coverBy)==0
+        assert cell._points==0
+
+    def test_targetCell_addBalloon(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        balloon = Balloon(cell)
+        cell.addBalloon(balloon)
+        assert balloon in cell.coverBy and len(cell.coverBy)==1
+
+    def test_targetCell_addBalloon(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        balloon = Balloon(cell)
+        cell.addBalloon(balloon)
+        cell.removeBalloon(balloon)
+        assert len(cell.coverBy)==0
+        with pytest.raises(AssertionError):
+            cell.removeBalloon(balloon)
+
+    def test_targetCell_isCovered(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        assert cell.isCovered()==False
+        balloon = Balloon(cell)
+        cell.addBalloon(balloon)
+        assert cell.isCovered()==True
+        cell.removeBalloon(balloon)
+        assert cell.isCovered()==False
+
+    def test_targetCell_incrPoints(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        assert cell._points==0
+        cell.incrPoints()
+        assert cell._points==1
+
+    def test_targetCell_getPoints(self):
+        x,y = 0,0
+        winds = [Wind(0,0)]
+        cell = TargetCell(x,y,winds)
+        assert cell.getPoints()==0
+        cell.incrPoints()
+        assert cell.getPoints()==1
