c1385e811b49e54ffe3ff0353440485347ca1e31
Tom Daniaud <tom.daniaud@etu.univ-nantes.fr>
2024-11-26 22:45:01 +0100

    minor bug fixing: correcting tuple type issue in test_targetCell.py

--- tests/test_targetCell.py
+++ tests/test_targetCell.py
@@ -5,7 +5,7 @@ class TestTargetCell:
 
     def test_targetCell_init(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         assert type(cell) == TargetCell
         assert len(cell.coverBy)==0
@@ -13,15 +13,15 @@ class TestTargetCell:
 
     def test_targetCell_addBalloon(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         balloon = Balloon(cell)
         cell.addBalloon(balloon)
         assert balloon in cell.coverBy and len(cell.coverBy)==1
 
-    def test_targetCell_addBalloon(self):
+    def test_targetCell_delBalloon(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         balloon = Balloon(cell)
         cell.addBalloon(balloon)
@@ -32,7 +32,7 @@ class TestTargetCell:
 
     def test_targetCell_isCovered(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         assert cell.isCovered()==False
         balloon = Balloon(cell)
@@ -43,7 +43,7 @@ class TestTargetCell:
 
     def test_targetCell_incrPoints(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         assert cell._points==0
         cell.incrPoints()
@@ -51,7 +51,7 @@ class TestTargetCell:
 
     def test_targetCell_getPoints(self):
         x,y = 0,0
-        winds = tuple([Wind(0,0)])
+        winds = Wind(0,0),
         cell = TargetCell(x,y,winds)
         assert cell.getPoints()==0
         cell.incrPoints()
