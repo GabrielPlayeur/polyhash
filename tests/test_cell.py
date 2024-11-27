65cd8f3181cf84d4ffda6512f30e99aa90c15497
Tom Daniaud <tom.daniaud@etu.univ-nantes.fr>
2024-11-27 17:51:37 +0100

    Updating every test with the row,col coordinate system

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -5,26 +5,26 @@ from objects.wind import Wind
 class TestCell:
 
     def test_cell_init(self):
-        x,y = 0,0
-        winds = tuple([Wind(0,0)])
-        cell = Cell(x,y,winds)
+        row,col = 0,0
+        winds = Wind(0,0),
+        cell = Cell(row,col,winds)
         assert type(cell) == Cell
-        assert cell.x==x and cell.y==y and cell.pos==(x,y)
+        assert cell.row==row and cell.col==col and cell.pos==(row,col)
         assert len(cell._winds)==1
 
     def test_cell_addTarget(self):
-        x,y = 0,0
-        winds = tuple([Wind(0,0)])
-        cell = Cell(x,y,winds)
+        row,col = 0,0
+        winds = Wind(0,0),
+        cell = Cell(row,col,winds)
         target = Cell(0,0,winds)
         cell.addTarget(target)
         assert len(cell.targets)==1
         assert cell.targets[0] is target
 
     def test_cell_getWinds(self):
-        x,y = 0,0
-        winds = tuple([Wind(0,0)])
-        cell = Cell(x,y,winds)
+        row,col = 0,0
+        winds = Wind(0,0),
+        cell = Cell(row,col,winds)
         assert cell.getWinds(1)==winds[0]
         with pytest.raises(AssertionError):
             cell.getWinds(0)
