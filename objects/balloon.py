ffb96f1a598b0737de4dc7d746d914ca7fb627d6
Gabriel <teigne.gabriel@gmail.com>
2024-11-28 16:18:12 +0100

    Modify polyparser to add altitude 0 to the wind vector, change Cell winds type

--- objects/balloon.py
+++ objects/balloon.py
@@ -6,9 +6,9 @@ from objects import Cell
 class Balloon:
     def __init__(self, startCell: Cell) -> None:
         """Entity that describe a balloon and it's altitude"""
-        self.cell: Cell | None = startCell
+        self.cell: Cell = startCell
         self.alt: int = 0
-        self.altMax = len(self.cell._winds)
+        self.altMax = len(self.cell._winds)-1
 
     def moveAlt(self, value: int) -> None:
         """Change the altitude of the balloon
@@ -16,11 +16,11 @@ class Balloon:
         args:
             value: incr, decr, don't move (1,-1,0) and must match (alt+val<=altMax) and (alt+val>0)
         """
+        if self.alt==0 and value==0: return
         assert value==1 or value==0 or value==-1
         assert self.altMax >= self.alt+value > 0
         self.alt += value
 
-    def applyWind(self, ) -> None:
+    def applyWind(self) -> None: #TODO: add test
         """Apply the wind at the current altitude"""
-        if self.alt > 0:
-            self.cell = self.cell.getNeighbor(self.alt)
+        self.cell = self.cell.getNeighbor(self.alt)
--- objects/cell.py
+++ objects/cell.py
@@ -4,36 +4,38 @@ from __future__ import annotations
 from .wind import Wind
 
 class Cell:
-    def __init__(self, row: int, col: int, winds: tuple[Wind]) -> None:
+    def __init__(self, row: int, col: int, winds: list[Wind]) -> None:
         """Entity that describe the wind and the target cell in range of a position"""
-        self.pos: tuple[int,int]          = (row,col)
+        self.pos: tuple[int,int] = (row,col)
         self.row: int = row
         self.col: int = col
-        self._winds: tuple[Wind]          = winds
-        self.targets: list[Cell]          = []
-        self.neighbors: list[Cell|None]   = []
+        self._winds: list[Wind] = winds
+        self.targets: list[Cell] = []
+        self.neighbors: list[Cell] = []
 
     def addTarget(self, target: Cell) -> None:
         """Add target cell into the cell's targets list"""
         self.targets.append(target)
 
-    def addNeighbor(self, cell:Cell|None) -> None:
-        "add a neighbor at the n+1 altitude, letting n be lenght of neighbors"
+    def addNeighbor(self, cell: Cell) -> None:
+        "add a neighbor at the n altitude, letting n be lenght of neighbors"
         self.neighbors.append(cell)
-    
 
-    def getNeighbor(self, alt: int) -> Cell|None:
-        assert len(self._winds)+1 > alt > 0
-        return self.neighbors[alt-1]
+    def getNeighbor(self, alt: int) -> Cell: #TODO: add test
+        assert len(self._winds) > alt >= 0
+        return self.neighbors[alt]
 
-    def getWinds(self, alt: int) -> Wind:
+    def getWinds(self) -> list[Wind]:
+        return self._winds
+
+    def getWindsByAlt(self, alt: int) -> Wind:
         """Get wind at the altitude
 
         args:
             alt: choosen altitude (0<alt<maxAlt)
         """
-        assert len(self._winds)+1 > alt > 0
-        return self._winds[alt-1]
-    
+        assert len(self._winds) > alt >= 0
+        return self._winds[alt]
+
     def __str__(self) -> str:
         return f"Cell ({self.row}:{self.col})"
