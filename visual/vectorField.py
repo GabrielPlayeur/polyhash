6ec60c46b00ab604fc961b10b87f7df2f1f8a8da
Gabriel <teigne.gabriel@gmail.com>
2024-12-03 15:45:08 +0100

    Update vectorField to match new cell winds

--- visual/vectorField.py
+++ visual/vectorField.py
@@ -15,8 +15,8 @@ def create(challenge: str) -> None:
             for j in range(cellMap.columns):
                 X.append(j)
                 Y.append(i)
-                U.append(cellMap.map[i][j].getWinds(alt_idx).dx)
-                V.append(cellMap.map[i][j].getWinds(alt_idx).dy)
+                U.append(cellMap.map[i][j].getWindsByAlt(alt_idx).dCol)
+                V.append(cellMap.map[i][j].getWindsByAlt(alt_idx).dRow)
         plt.figure(figsize=(6, 6))
         plt.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, color='blue')
         plt.title(f'Champs de vecteurs à altitude {alt_idx}')
@@ -29,8 +29,8 @@ def create(challenge: str) -> None:
 
 if __name__ == '__main__':
     name = "a_example"
-    name = "b_small"
     name = "c_medium"
     name = "d_final"
+    name = "b_small"
 
     create(name)
