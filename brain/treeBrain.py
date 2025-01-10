5ba49109855c92d1a61b1cbddd47e36abc9e3bc2
Florian PETIOT <florian.petiot44@gmail.com>
2025-01-10 12:59:22 +0100

    refactor TreeBrain scoring logic and update covered targets

--- brain/treeBrain.py
+++ brain/treeBrain.py
@@ -101,18 +101,23 @@ class TreeBrain:
     def _pointForNode(self, cell:Cell, stage:int) -> int:
         """Compute the number of point for a node"""
         points = 0
-        if exploredAtStage:=self.coveredTarget.get(stage):
-            for target in cell.targets:
-                if target not in exploredAtStage:
-                    points += 1
-                else:
-                    self.coveredTarget[stage].remove(target)
+        exploredAtStage = self.coveredTarget.get(stage, set())
 
-        return points or len(cell.targets)
+        for target in cell.targets:
+            if target not in exploredAtStage:
+                points += 1
+            else:
+                exploredAtStage.remove(target)
+        # Update coveredTarget after iteration
+        self.coveredTarget[stage] = exploredAtStage
+        return points
         
     def bestPath(self, leaves:list[Node]) -> deque[Node]:
         """Return the best path of the tree. Use self.construct() to generate leaves"""
-        node = leaves.pop()
+        
+        # take the best leaf with largest sum
+        node = max(leaves, key=lambda x: x.sum)
+        
         path  = deque([node])
         while node.hasParent():     # type: ignore
             node = node.parent      # type: ignore
@@ -164,5 +169,5 @@ if __name__ == "__main__":
     parser = parseChallenge(f'challenges/{name}.in')
     graph = CellMap(parser)
 
-    tr = TreeBrain(graph, 500, debugInfo=True)
+    tr = TreeBrain(graph, 100000, debugInfo=True)
     tr.solve()
